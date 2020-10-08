2c001067eaf952d00ef9ef50e5c7ce9ca6bf9470
Johnny Gill <swfiua@gmail.com>
2020-10-08 14:33:26 -0400

    basic version of dem now working

--- noah/dem.py
+++ noah/dem.py
@@ -16,29 +16,30 @@ class Dem(magic.Ball):
 
     async def start(self):
 
-        self.band = gdal.Open(self.filename).GetRasterBand(0)
-
-        self.xoff = 0
-        self.yoff = 0
+        self.tif = gdal.Open(self.filename)
+        return
 
     async def run(self):
 
+        # get a square from the image
+        print(self.xoff, self.yoff, self.size)
+        band = self.tif.GetRasterBand(1)
         square = band.ReadAsArray(
             xoff = self.xoff,
             yoff = self.yoff,
             win_xsize = self.size,
-            win_ysize = self.size,)
-
+            win_ysize = self.size)
+        print(square)
         plt.imshow(square)
 
-        self.put()
+        await self.put()
 
         self.xoff += self.size
-        if self.xoff > self.band.Xsize:
+        if self.xoff > band.XSize - self.size:
             self.xoff = 0
             self.yoff += self.size
 
-            if self.yoff >= self.size:
+            if self.yoff >= band.YSize - self.size:
                 self.yoff = 0
 
 async def run(args):
@@ -62,6 +63,8 @@ if __name__ == '__main__':
 
     parser.add_argument('-filename')
     parser.add_argument('-size', type=int, default=1000)
+    parser.add_argument('-xoff', type=int, default=0)
+    parser.add_argument('-yoff', type=int, default=0)
 
     args = parser.parse_args()
 
